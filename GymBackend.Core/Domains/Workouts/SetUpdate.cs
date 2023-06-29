b004260f8c516750d9249a25eb88c3025beffdec
Piers Taylor <piers.taylorr@gmail.com>
2023-06-29 16:50:32 +0100

    Revert "Revert "Breaking and reinventing of everything to match sessionStorage focused frontend""
    This reverts commit be4b8ee17b172610ebc92ed640bdf47c4a61979f.

--- GymBackend.Core/Domains/Workouts/SetUpdate.cs
+++ GymBackend.Core/Domains/Workouts/SetUpdate.cs
@@ -2,10 +2,17 @@
 {
     public class SetUpdate
     {
-        public Guid Id { get; set; }
-        public MuscleGroup MuscleGroup { get; set; }
         public string Weight { get; set; }
         public int Sets { get; set; }
         public int Reps { get; set; }
+        public int Order { get; set; }
+
+        public SetUpdate(string weight, int sets, int reps, int order)
+        {
+            Weight = weight;
+            Sets = sets;
+            Reps = reps;
+            Order = order;
+        }
     }
 }
