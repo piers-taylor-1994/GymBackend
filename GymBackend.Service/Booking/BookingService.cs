a47c4042bac80f47ef897226f3eb5234b6e538fe
Piers Taylor <piers.taylorr@gmail.com>
2024-07-02 22:09:43 +0100

    Changed the code to show the next few weeks wednesdays

--- GymBackend.Service/Booking/BookingService.cs
+++ GymBackend.Service/Booking/BookingService.cs
@@ -20,19 +20,42 @@ namespace GymBackend.Service.Booking
 
         public async Task<List<BookingItem>> GetTimetable()
         {
-            int daysToAdd = ((int)DayOfWeek.Wednesday - (int)DateTime.Today.DayOfWeek + 7) % 7;
-            var nextWednesday = DateTime.Today.AddDays(daysToAdd);
-            var url = new Uri("https://better-admin.org.uk/api/activities/venue/the-hive-leisure-centre/activity/fitness-classes-c/timetable?date=" + nextWednesday.ToString("yyyy-MM-dd"));
+            var date = DateTime.UtcNow;
+            var requestMessages = new List<HttpRequestMessage>();
+            var bookings = new List<BookingItem>();
 
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "Bearer v4.local.uk5TgNXsHIaK9lQ7dxNagw5YH-aShddDrzF_-VojO8fAYKiyhoZsEJP5vGrEl6cyfCkJVNcVmnBHXae2TAbA2nkHk3sIgVHSW4PazLADp8a55w07R-5agYwY_FBZ2wPe0hc_6Gdmqda_iI1pXJWYO40VRg-5bWPKrMq8mGM--RbABGzBeVy2JaIut4QQ-OvXy5vx0lJQDuwOxjI1");
-            request.Headers.Add("Origin", "https://bookings.better.org.uk");
+            // This is for every wednesday for x weeks
+            for (var i = 0; i < 3; i++)
+            {
+                int daysToAdd = ((int)DayOfWeek.Wednesday - (int)DateTime.Today.DayOfWeek + 7) % 7;
+                var nextWednesday = DateTime.Today.AddDays(daysToAdd + i * 7);
+                var newRequest = new HttpRequestMessage(HttpMethod.Get, new Uri("https://better-admin.org.uk/api/activities/venue/the-hive-leisure-centre/activity/fitness-classes-c/timetable?date=" + nextWednesday.ToString("yyyy-MM-dd")));
 
-            var response = await httpClient.SendAsync(request, CancellationToken.None);
-            string responseString = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<Deserialize.Root>(responseString);
+                newRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "Bearer v4.local.uk5TgNXsHIaK9lQ7dxNagw5YH-aShddDrzF_-VojO8fAYKiyhoZsEJP5vGrEl6cyfCkJVNcVmnBHXae2TAbA2nkHk3sIgVHSW4PazLADp8a55w07R-5agYwY_FBZ2wPe0hc_6Gdmqda_iI1pXJWYO40VRg-5bWPKrMq8mGM--RbABGzBeVy2JaIut4QQ-OvXy5vx0lJQDuwOxjI1");
+                newRequest.Headers.Add("Origin", "https://bookings.better.org.uk");
 
-            return result.Data.Where(r => r.Spaces > 0).ToList();
+                requestMessages.Add(newRequest);
+            }
+
+            //This is for the next 7 days
+            //for (var i = 0; i < 8; i++)
+            //{
+            //    var newRequest = new HttpRequestMessage(HttpMethod.Get, new Uri("https://better-admin.org.uk/api/activities/venue/the-hive-leisure-centre/activity/fitness-classes-c/timetable?date=" + date.AddDays(i).ToString("yyyy-MM-dd")));
+            //    newRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "Bearer v4.local.uk5TgNXsHIaK9lQ7dxNagw5YH-aShddDrzF_-VojO8fAYKiyhoZsEJP5vGrEl6cyfCkJVNcVmnBHXae2TAbA2nkHk3sIgVHSW4PazLADp8a55w07R-5agYwY_FBZ2wPe0hc_6Gdmqda_iI1pXJWYO40VRg-5bWPKrMq8mGM--RbABGzBeVy2JaIut4QQ-OvXy5vx0lJQDuwOxjI1");
+            //    newRequest.Headers.Add("Origin", "https://bookings.better.org.uk");
+
+            //    requestMessages.Add(newRequest);
+            //}
+
+            foreach (var request in requestMessages)
+            {
+                var response = await httpClient.SendAsync(request, CancellationToken.None);
+                string responseString = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<Deserialize.Root>(responseString);
+                if (result != null && result.Data != null) bookings = bookings.Concat(result.Data.Where(r => r.Spaces > 0 && r.Name.Contains("yoga", StringComparison.CurrentCultureIgnoreCase))).ToList();
+            }
+
+            return bookings;
         }
 
         public async Task<string> CreateBookingAsync(Guid userId, int bookingId)
@@ -58,7 +81,7 @@ namespace GymBackend.Service.Booking
         {
             var url = new Uri("https://better-admin.org.uk/api/activities/cart/add");
 
-            string body = "{\"items\":[{\"id\":\""+bookingId+"\",\"type\":\"activity\",\"pricing_option_id\":1,\"apply_benefit\":true,\"activity_restriction_ids\":[]}],\"membership_user_id\":849218,\"selected_user_id\":null}";
+            string body = "{\"items\":[{\"id\":\"" + bookingId + "\",\"type\":\"activity\",\"pricing_option_id\":1,\"apply_benefit\":true,\"activity_restriction_ids\":[]}],\"membership_user_id\":849218,\"selected_user_id\":null}";
 
             var request = new HttpRequestMessage(HttpMethod.Post, url);
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", auth);
